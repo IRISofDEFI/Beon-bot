7b187124a50f4e57f40c374093729a22e3faa8cf
IRIS <irisofdefi@gmail.com>
2026-02-06 16:33:03 +0100

    Move orders and tracking page to React

--- Beon MarketPlace/src/App.jsx
+++ Beon MarketPlace/src/App.jsx
@@ -1,8 +1,10 @@
-import { HomePage } from './pages/HomePage'
-import { Routes, Route } from 'react-router'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { HomePage } from './pages/HomePage';
+import { Routes, Route } from 'react-router';
+import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 
-import './App.css'
+import './App.css';
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route index element={<HomePage />} />
           <Route path='checkout' element={<CheckoutPage />} />
+          <Route path='orders'  element={<OrdersPage />} />
+          <Route path='tracking' element={<TrackingPage />} />
         </Routes>
       
       </>
