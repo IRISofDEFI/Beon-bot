50832c21841dcb6531fde3c33c56493a5684b3aa
IRIS <irisofdefi@gmail.com>
2026-02-07 12:56:39 +0100

    Add server config for api and images

--- Beon MarketPlace/src/components/Header.jsx
+++ Beon MarketPlace/src/components/Header.jsx
@@ -1,42 +1,44 @@
-    import { Link } from 'react-router';
-    import './header.css';
+import { Link } from 'react-router';
+import './header.css';
 
-export function Header({cart}) {
-    let totalQuantity = 0;
+export function Header({ cart }) {
+let totalQuantity = 0;
 
-    cart.forEach((cartItem) => {
-        totalQuantity += cartItem.quantity;
-    });
+cart.forEach((cartItem) => {
+totalQuantity += cartItem.quantity;
+});
 
 return (
-        <div className="header">
+<div className="header">
     <div className="left-section">
-        <Link to="/" className="header-link">
-        <img className="logo"
-        src="images/logo-white.png" />
-        <img className="mobile-logo"
-        src="images/mobile-logo-white.png" />
-        </Link>
+    <Link to="/" className="header-link">
+        <img className="logo" src="images/logo-white.png" />
+        <img className="mobile-logo" src="images/mobile-logo-white.png" />
+    </Link>
     </div>
 
     <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+    <input className="search-bar" type="text" placeholder="Search" />
 
-        <button className="search-button">
+    <button className="search-button">
         <img className="search-icon" src="images/icons/search-icon.png" />
-        </button>
+    </button>
     </div>
 
     <div className="right-section">
-        <Link className="orders-link header-link" to="/orders">
-            <span className="orders-text">Orders</span>
-        </Link>
+    <Link className="orders-link header-link" to="/orders">
+        <span className="orders-text">Orders</span>
+    </Link>
 
-        <a className="cart-link header-link" href="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">{totalQuantity}</div>
-            <div className="cart-text">Cart</div>
-        </a>
+    {/* ✅ Link instead of <a> */}
+    <Link className="cart-link header-link" to="/checkout">
+        <img className="cart-icon" src="images/icons/cart-icon.png" />
+
+        {/* ✅ actually show the quantity */}
+        <div className="cart-quantity">{totalQuantity}</div>
+
+        <div className="cart-text">Cart</div>
+    </Link>
     </div>
 </div>
 );
